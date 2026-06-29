e28672c4551148285058893f656d892931de1ea9
Fadli <fadli.fathur@proenergi.co.id>
2026-06-29 14:34:22 +0700

    update

--- database/seeders/MasterDokumenPendukungSeeder.php
+++ database/seeders/MasterDokumenPendukungSeeder.php
@@ -4,83 +4,81 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class MasterDokumenPendukungSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('master_dokumen_pendukung')->insert([
+        $documents = [
             [
-                'id' => 1,
                 'nama_dokumen' => 'Surat Referensi Bank / Surat Pernyataan No. Rekening dari Direktur',
-                'slug' => Str::slug('Surat Referensi Bank'),
+                'slug' => 'surat-referensi-bank',
                 'deskripsi' => '(wajib)',
-                'is_required' => true
+                'is_required' => true,
             ],
             [
-                'id' => 2,
                 'nama_dokumen' => 'Surat Pernyataan Pembayaran PPN Bermeterai',
-                'slug' => Str::slug('Surat Pernyataan PPN'),
+                'slug' => 'surat-pernyataan-ppn',
                 'deskripsi' => '(wajib jika PKP, format terlampir)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 3,
                 'nama_dokumen' => 'SIUPAL',
-                'slug' => Str::slug('SIUPAL'),
+                'slug' => 'siupal',
                 'deskripsi' => '(Jika Perusahaan Pelayaran)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 4,
                 'nama_dokumen' => 'SIUJK / SBUJK',
-                'slug' => Str::slug('SIUJK SBUJK'),
+                'slug' => 'siujk-sbujk',
                 'deskripsi' => '(Jika Perusahaan Konstruksi)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 5,
                 'nama_dokumen' => 'Fotokopi E-KTP',
-                'slug' => Str::slug('E-KTP'),
+                'slug' => 'e-ktp',
                 'deskripsi' => '(Jika Orang Pribadi / Perorangan)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 6,
                 'nama_dokumen' => 'Lampiran Surat Pernyataan Non PKP Bermeterai',
-                'slug' => Str::slug('Non PKP'),
+                'slug' => 'non-pkp',
                 'deskripsi' => '(Jika Non PKP)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 7,
                 'nama_dokumen' => 'Fotokopi SPPKP',
-                'slug' => Str::slug('SPPKP'),
+                'slug' => 'sppkp',
                 'deskripsi' => null,
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 8,
                 'nama_dokumen' => 'Lampiran Surat Pernyataan Non NPWP Bermeterai',
-                'slug' => Str::slug('Non NPWP'),
+                'slug' => 'non-npwp',
                 'deskripsi' => '(Jika tidak melampirkan NPWP)',
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 9,
                 'nama_dokumen' => 'Fotokopi NPWP',
-                'slug' => Str::slug('NPWP'),
+                'slug' => 'npwp',
                 'deskripsi' => null,
-                'is_required' => false
+                'is_required' => false,
             ],
             [
-                'id' => 10,
                 'nama_dokumen' => 'Dokumen Legalitas',
-                'slug' => Str::slug('Dokumen Legalitas'),
+                'slug' => 'dokumen-legalitas',
                 'deskripsi' => '(Akta Pendirian, Akta Terbaru, SIUP, NIB, TDP, Dokumen Pendukung Lainnya)',
-                'is_required' => false
+                'is_required' => false,
             ],
-        ]);
+        ];
+
+        foreach ($documents as $document) {
+            DB::table('master_dokumen_pendukung')->updateOrInsert(
+                [
+                    'slug' => $document['slug'],
+                ],
+                $document
+            );
+        }
     }
 }
